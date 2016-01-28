43d52975b53981fe534ce0f189aa8fd5cdf3041e
Santos <santos82h@gmail.com>
2016-01-28 22:00:28 +0100

    .sh

--- mkgit.sh
+++ mkgit.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-if [ -f "~/.github" ]; then
-	echo "~/.github don't find"
+read -p "User: " -e -i "$(git config --global user.name)" USER
+if [ -z "$USER" ]; then
+	echo "User is required"
 	exit 1
 fi
-CONFIG=$(cat ~/.github)
-USER=$(echo "$CONFIG" | cut -f1 -d' ')
-PASS=$(echo "$CONFIG" | cut -f2 -d' ')
-TOKEN=$(echo "$CONFIG" | cut -f3 -d' ')
+read -s -p "Password: " PASS
+echo ""
+if [ -z "$PASS" ]; then
+	echo "Password is required"
+	exit 1
+fi
+
 REPO=${PWD##*/}
+
+OK=$(curl -w "%{http_code}" -o /dev/null -s -u "$USER:$PASS" https://api.github.com)
+
+if [ "$OK" -ne "200" ]; then
+	echo "Login error: $OK"
+	exit 1
+fi
 
 curl -s -u "$USER:$PASS" https://api.github.com/user/repos -d "{\"name\": \"$REPO\"}"
 
 git init
 git add *
-git commit -m "first commit"
+git commit -m "First commit"
 git remote add origin https://github.com/${USER}/${REPO}.git
+#git remote add origin git@github.com:${USER}/${REPO}.git
 git push -u origin master
+
